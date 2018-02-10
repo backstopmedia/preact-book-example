ad38e0228481f89183c256c195072785f007dd43
Vu Tran <vutran@dropbox.com>
2018-02-10 12:17:59 -0800

    update styling

--- src/components/header/index.js
+++ src/components/header/index.js
@@ -6,7 +6,7 @@ export default class Header extends Component {
 	render() {
 		return (
 			<header class={style.header}>
-				<h1>Preact Book Example</h1>
+				<h1>Preact HNPWA</h1>
 				<nav>
 					<Link activeClassName={style.active} href="/">Top</Link>
 					<Link activeClassName={style.active} href="/new">New</Link>
