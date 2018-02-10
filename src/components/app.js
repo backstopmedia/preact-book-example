bd4bc84f4973124fdba1f9c173e56a7e17021e01
Vu Tran <vutran@dropbox.com>
2018-02-09 22:20:04 -0800

    Remove profile route

--- src/components/app.js
+++ src/components/app.js
@@ -1,11 +1,7 @@
 import { h, Component } from 'preact';
 import { Router } from 'preact-router';
-
 import Header from './header';
 import Home from '../routes/home';
-import Profile from '../routes/profile';
-// import Home from 'async!../routes/home';
-// import Profile from 'async!../routes/profile';
 
 if (module.hot) {
 	require('preact/debug');
@@ -26,8 +22,6 @@ export default class App extends Component {
 				<Header />
 				<Router onChange={this.handleRoute}>
 					<Home path="/" />
-					<Profile path="/profile/" user="me" />
-					<Profile path="/profile/:user" />
 				</Router>
 			</div>
 		);
---
+++ src/hn.js
@@ -0,0 +1,2 @@
+// https://github.com/HackerNews/API#uri-and-versioning
+export const HN_API_URI = 'https://hacker-news.firebaseio.com/v0/';
